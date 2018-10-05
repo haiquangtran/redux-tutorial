6d29b1bffa23c8862a2ca54184398a010091297b
haiquangtran <hai.quang.tran@gmail.com>
2018-10-06 02:20:09 +1000

    added boostrap file

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ArticleList from './ArticleList';
+import ArticleForm from './ArticleForm';
 
 const App = () => (
   <div className="row mt-5">
     <div className="col-md-4 offset-md-1">
       <h2>Articles</h2>
       <ArticleList />
+    </div>
+    <div className="col-md-4 offset-md-1">
+      <h2>Add a new article</h2>
+      <ArticleForm />
     </div>
   </div>
 );
